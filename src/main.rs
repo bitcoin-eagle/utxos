9bac7ccaa7c46b80a1252a3f71778c287cbb4962
Bitcoin Eagle <hello@bitcoineagle.dev>
2023-02-13 16:41:34 +0700

    PE-4: connect to rpc

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
-use bitcoincore_rpc::Client;
+use bitcoincore_rpc::{Client, RpcApi};
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> anyhow::Result<()> {
+    let c = Client::new(
+        "localhost:8332",
+        bitcoincore_rpc::Auth::UserPass("rust-utxos".into(), "o4ka4wx3i0wxar0bec2w1sm9h".into()),
+    )?;
+    let hash = c.get_best_block_hash()?;
+    println!("best block hash: {}", hash);
+    println!("block info: {:?}", c.get_block_header_info(&hash)?);
+    Ok(())
 }
